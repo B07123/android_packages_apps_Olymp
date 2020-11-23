0b2b0540512439b3a144f4662409ec32eb698734
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-11-23 19:51:38 +0100

    ZenHub: Toggle QS Footer security information [2/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

---
+++ src/com/zen/hub/fragments/subs/QsFooterItems.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2020 ZenX-OS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.zen.hub.fragments.subs;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+import android.content.ContentResolver;
+import androidx.preference.*;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class QsFooterItems extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.qs_footer_items);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ZENX_SETTINGS;
+    }
+
+}
