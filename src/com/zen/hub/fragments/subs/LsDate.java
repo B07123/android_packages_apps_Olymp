230f0d91adba8bbc853929536ce040ff397dbea2
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-11-24 18:37:34 +0100

    ZenHub: Lockscreen Date Fonts [2/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

---
+++ src/com/zen/hub/fragments/subs/LsDate.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (C) 2020 ZenX-OS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.zen.hub.fragments.subs;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.database.ContentObserver;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.*;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LsDate extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String LOCK_DATE_FONTS = "lock_date_fonts";
+    private ListPreference mLockDateFonts;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.ls_date);
+
+        // Lockscren Date Fonts
+        mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
+        mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 32)));
+        mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+        mLockDateFonts.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLockDateFonts) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_DATE_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockDateFonts.setValue(String.valueOf(newValue));
+            mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ZENX_SETTINGS;
+    }
+
+}
