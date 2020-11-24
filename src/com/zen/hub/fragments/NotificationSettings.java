a646e5b40a2d8fcfc0d2166ca79c970dd6f17bbf
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-11-24 19:31:18 +0100

    ZenHub: incall vibration options [3/3]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- src/com/zen/hub/fragments/NotificationSettings.java
+++ src/com/zen/hub/fragments/NotificationSettings.java
@@ -19,22 +19,47 @@ package com.zen.hub.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.os.Bundle;
-import com.android.settings.R;
-import androidx.preference.*;
-import android.content.ContentResolver;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.android.settings.R;
+import com.zen.hub.utils.Utils;
+
 import com.android.settings.SettingsPreferenceFragment;
+import com.zenx.support.colorpicker.ColorPickerPreference;
+import com.zenx.support.preferences.CustomSeekBarPreference;
+import com.zenx.support.preferences.SystemSettingSeekBarPreference;
 
 public class NotificationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.zen_hub_notifications);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
