04eb576e65e9c3087b50574f0581f4e9afc8cbd5
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-11-25 11:58:52 +0100

    ZenHub: Allow customizing footer text [2/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- src/com/zen/hub/fragments/subs/QsFooterItems.java
+++ src/com/zen/hub/fragments/subs/QsFooterItems.java
@@ -24,22 +24,51 @@ import com.android.settings.R;
 import android.content.ContentResolver;
 import androidx.preference.*;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import com.zenx.support.preferences.SystemSettingEditTextPreference;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 public class QsFooterItems extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String X_FOOTER_TEXT_STRING = "x_footer_text_string";
+    private SystemSettingEditTextPreference mFooterString;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.qs_footer_items);
+
+        mFooterString = (SystemSettingEditTextPreference) findPreference(X_FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                X_FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("ZenX -Feel the Power");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.X_FOOTER_TEXT_STRING, "ZenX -Feel the Power");
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-
+        if (preference == mFooterString) {
+                String value = (String) newValue;
+                if (value != "" && value != null)
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.X_FOOTER_TEXT_STRING, value);
+                else {
+                    mFooterString.setText("ZenX -Feel the Power");
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.X_FOOTER_TEXT_STRING, "ZenX -Feel the Power");
+                }
+                return true;
+            }
         return false;
     }
 
