944db2c4e230f2ed2ce9bff7df9c5a84afa94fb4
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-01-22 13:51:48 +0100

    ZenHub Add color mode and random color option with interval picker [1/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- src/com/zen/hub/fragments/subs/SbLogo.java
+++ src/com/zen/hub/fragments/subs/SbLogo.java
@@ -37,23 +37,93 @@ import androidx.preference.*;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.zenx.support.preferences.SystemSettingListPreference;
 import com.zenx.support.preferences.CustomSeekBarPreference;
+import com.zenx.support.colorpicker.ColorPickerPreference;
 
 public class SbLogo extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_LOGO_COLOR_MODE = "status_bar_logo_color_mode";
+    private static final String STATUSBAR_LOGO_RANDOM_COLOR_INTERVAL = "statusbbar_logo_random_color_interval";
+    private static final String STATUS_BAR_LOGO_COLOR = "status_bar_logo_color";
+
+    private SystemSettingListPreference mLogoMode;
+    private CustomSeekBarPreference mLogoColorInterval;
+    private ColorPickerPreference mLogoColor;
+
+    private static final int DEFAULT_COLOR = 0xffffffff;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.sb_logo);
+
+        mLogoMode = (SystemSettingListPreference) findPreference(STATUS_BAR_LOGO_COLOR_MODE);
+        int mode = Settings.System.getIntForUser(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_COLOR_MODE, 0, UserHandle.USER_CURRENT);
+        mLogoMode.setValue(String.valueOf(mode));
+        mLogoMode.setSummary(mLogoMode.getEntry());
+        mLogoMode.setOnPreferenceChangeListener(this);
+
+        mLogoColorInterval = (CustomSeekBarPreference) findPreference(STATUSBAR_LOGO_RANDOM_COLOR_INTERVAL);
+        int time = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUSBAR_LOGO_RANDOM_COLOR_INTERVAL, 3);
+        mLogoColorInterval.setValue(time);
+        mLogoColorInterval.setOnPreferenceChangeListener(this);
+
+        mLogoColor = (ColorPickerPreference) findPreference(STATUS_BAR_LOGO_COLOR);
+        mLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_COLOR, DEFAULT_COLOR);
+        String hexColor = String.format("#%08x", (DEFAULT_COLOR & intColor));
+        mLogoColor.setSummary(hexColor);
+        mLogoColor.setNewPreviewColor(intColor);
+        handlePreferenceVisibilty(mode);
+    }
+
+    private void handlePreferenceVisibilty(int mode) {
+        switch (mode) {
+            case 0:
+                mLogoColorInterval.setVisible(false);
+            case 1:
+                mLogoColorInterval.setVisible(false);
+                break;
+            case 2:
+                mLogoColorInterval.setVisible(true);
+                break;
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLogoMode) {
+            int val = Integer.parseInt((String) newValue);
+            int index = mLogoMode.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_COLOR_MODE, val);
+            mLogoMode.setSummary(mLogoMode.getEntries()[index]);
+            handlePreferenceVisibilty(val);
+            return true;
+        } else if (preference == mLogoColorInterval) {
+            int time = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_LOGO_RANDOM_COLOR_INTERVAL, time);
+            return true;
+        } else if (preference == mLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_COLOR, intHex);
+            return true;
+        }
         return false;
     }
 
