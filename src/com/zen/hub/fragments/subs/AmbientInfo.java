d804178c75767b2148855c558603d5de48816a00
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-02-07 10:44:50 +0100

    ZenHub: Add additional ambient info
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

---
+++ src/com/zen/hub/fragments/subs/AmbientInfo.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2020 ZenX-OS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.zen.hub.fragments.subs;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.database.ContentObserver;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.*;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.zenx.support.preferences.CustomSeekBarPreference;
+
+public class AmbientInfo extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.ambient_info);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ZENX_SETTINGS;
+    }
+
+}
