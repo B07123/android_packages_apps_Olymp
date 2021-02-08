6c220e619fb6cb3e728bcfbbbbcd2115d37f3a4e
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-02-08 16:01:04 +0100

    ZenHub: Extend QS battery options
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- src/com/zen/hub/fragments/subs/QsBatteryModes.java
+++ src/com/zen/hub/fragments/subs/QsBatteryModes.java
@@ -40,9 +40,17 @@ import android.widget.Toast;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.zenx.support.preferences.CustomSeekBarPreference;
+import com.zenx.support.preferences.SystemSettingSwitchPreference;
+import com.zenx.support.preferences.SystemSettingListPreference;
 
 public class QsBatteryModes extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String HIDE_PERCENTAGE_NEXT_TO_ESTIMATE = "hide_percentage_next_to_estimate";
+    private static final String QS_SHOW_BATTERY_PERCENT = "qs_show_battery_percent";
+
+    private  SystemSettingListPreference mQsBatteryPercentageMode;
+    private SystemSettingSwitchPreference mQsHidePercentageNextToEstimate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,10 +58,51 @@ public class QsBatteryModes extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.qs_battery_modes);
 
+        mQsBatteryPercentageMode = (SystemSettingListPreference) findPreference(QS_SHOW_BATTERY_PERCENT);
+        mQsBatteryPercentageMode.setOnPreferenceChangeListener(this);
+
+        mQsHidePercentageNextToEstimate = (SystemSettingSwitchPreference) findPreference(HIDE_PERCENTAGE_NEXT_TO_ESTIMATE);
+        mQsHidePercentageNextToEstimate.setOnPreferenceChangeListener(this);
+
+        handleVisibilty();
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsBatteryPercentageMode) {
+                int val = Integer.parseInt((String) newValue);
+                int index = mQsBatteryPercentageMode.findIndexOfValue((String) newValue);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_SHOW_BATTERY_PERCENT, val);
+                mQsBatteryPercentageMode.setSummary(mQsBatteryPercentageMode.getEntries()[index]);
+                handleVisibilty();
+                return true;
+       } else if (preference == mQsHidePercentageNextToEstimate) {
+            Boolean value = (Boolean) newValue;
+            mQsHidePercentageNextToEstimate.setChecked(value);
+            handleVisibilty();
+            return true;
+        }
         return false;
+    }
+
+    private void handleVisibilty() {
+        switch (getBatteryPercentageMode()) {
+            case 0:
+                mQsHidePercentageNextToEstimate.setVisible(false);
+                break;
+            case 1:
+                mQsHidePercentageNextToEstimate.setVisible(false);
+                break;
+            case 2:
+                mQsHidePercentageNextToEstimate.setVisible(true);
+                break;
+        }
+    }
+
+
+    private int getBatteryPercentageMode() {
+        return Settings.System.getInt(getContext().getContentResolver(),
+            Settings.System.QS_SHOW_BATTERY_PERCENT, 2);
     }
 
     @Override
