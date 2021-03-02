2f168404a2d11e5adeee6d26eb33d53eafbb4b1c
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-03-02 15:47:14 +0100

    ZenHub: Allow scheduling always on display [2/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- src/com/zen/hub/fragments/subs/Ambient.java
+++ src/com/zen/hub/fragments/subs/Ambient.java
@@ -27,6 +27,9 @@ import android.provider.Settings;
 import android.os.Bundle;
 import android.widget.Toast;
 import com.android.settings.R;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import android.app.TimePickerDialog;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -41,24 +44,32 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.zenx.support.colorpicker.ColorPickerPreference;
 import com.zenx.support.preferences.CustomSeekBarPreference;
 import com.zenx.support.preferences.SystemSettingSeekBarPreference;
+import com.zenx.support.preferences.SecureSettingListPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class Ambient extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+    Preference.OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
 
     private ColorPickerPreference mEdgeLightColorPreference;
     private SystemSettingSeekBarPreference mEdgeLightDurationPreference;
     private SystemSettingSeekBarPreference mEdgeLightRepeatCountPreference;
     private ListPreference mColorMode;
     private SystemSettingSeekBarPreference mEdgeLightTimeoutPreference;
+    private static final String MODE_KEY = "doze_always_on_auto_mode";
+    private static final String SINCE_PREF_KEY = "doze_always_on_auto_since";
+    private static final String TILL_PREF_KEY = "doze_always_on_auto_till";
 
     private static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
     private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
     private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
     private static final String PULSE_COLOR_MODE_PREF = "ambient_notification_light_color_mode";
     private static final String AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
+    
+    private SecureSettingListPreference mModePref;
+    private Preference mSincePref;
+    private Preference mTillPref;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -119,6 +130,21 @@ public class Ambient extends SettingsPreferenceFragment implements
         mColorMode.setValue(Integer.toString(value));
         mColorMode.setSummary(mColorMode.getEntry());
         mColorMode.setOnPreferenceChangeListener(this);
+
+        mSincePref = findPreference(SINCE_PREF_KEY);
+        mSincePref.setOnPreferenceClickListener(this);
+        mTillPref = findPreference(TILL_PREF_KEY);
+        mTillPref.setOnPreferenceClickListener(this);
+
+        int mode = Settings.Secure.getIntForUser(getContentResolver(),
+                MODE_KEY, 0, UserHandle.USER_CURRENT);
+        mModePref = (SecureSettingListPreference) findPreference(MODE_KEY);
+        mModePref.setValue(String.valueOf(mode));
+        mModePref.setSummary(mModePref.getEntry());
+        mModePref.setOnPreferenceChangeListener(this);
+
+        updateTimeEnablement(mode == 2);
+        updateTimeSummary(mode);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -173,8 +199,110 @@ public class Ambient extends SettingsPreferenceFragment implements
                         Settings.System.NOTIFICATION_PULSE_ACCENT, 0);
             }
             return true;
+        } else if (preference == mModePref) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mModePref.findIndexOfValue((String) newValue);
+            mModePref.setSummary(mModePref.getEntries()[index]);
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    MODE_KEY, value, UserHandle.USER_CURRENT);
+            updateTimeEnablement(value == 2);
+            updateTimeSummary(value);
         }
          return false;
+    }
+
+    public boolean onPreferenceClick(Preference preference) {
+        String[] times = getCustomTimeSetting();
+        boolean isSince = preference == mSincePref;
+        int hour, minute; hour = minute = 0;
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                updateTimeSetting(isSince, hourOfDay, minute);
+            }
+        };
+        if (isSince) {
+            String[] sinceValues = times[0].split(":", 0);
+            hour = Integer.parseInt(sinceValues[0]);
+            minute = Integer.parseInt(sinceValues[1]);
+        } else {
+            String[] tillValues = times[1].split(":", 0);
+            hour = Integer.parseInt(tillValues[0]);
+            minute = Integer.parseInt(tillValues[1]);
+        }
+        TimePickerDialog dialog = new TimePickerDialog(getContext(), listener,
+                hour, minute, DateFormat.is24HourFormat(getContext()));
+        dialog.show();
+        return true;
+    }
+
+    private String[] getCustomTimeSetting() {
+        String value = Settings.Secure.getStringForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_TIME, UserHandle.USER_CURRENT);
+        if (value == null || value.equals("")) value = "20:00,07:00";
+        return value.split(",", 0);
+    }
+
+    private void updateTimeEnablement(boolean enabled) {
+        mSincePref.setEnabled(enabled);
+        mTillPref.setEnabled(enabled);
+    }
+
+    private void updateTimeSummary(int mode) {
+        updateTimeSummary(getCustomTimeSetting(), mode);
+    }
+
+    private void updateTimeSummary(String[] times, int mode) {
+        if (mode == 0) {
+            mSincePref.setSummary("-");
+            mTillPref.setSummary("-");
+            return;
+        }
+        if (mode == 1) {
+            mSincePref.setSummary(R.string.always_on_display_schedule_sunset);
+            mTillPref.setSummary(R.string.always_on_display_schedule_sunrise);
+            return;
+        }
+        if (DateFormat.is24HourFormat(getContext())) {
+            mSincePref.setSummary(times[0]);
+            mTillPref.setSummary(times[1]);
+            return;
+        }
+        String[] sinceValues = times[0].split(":", 0);
+        String[] tillValues = times[1].split(":", 0);
+        int sinceHour = Integer.parseInt(sinceValues[0]);
+        int tillHour = Integer.parseInt(tillValues[0]);
+        String sinceSummary = "";
+        String tillSummary = "";
+        if (sinceHour > 12) {
+            sinceHour -= 12;
+            sinceSummary += String.valueOf(sinceHour) + ":" + sinceValues[1] + " PM";
+        } else {
+            sinceSummary = times[0].substring(1) + " AM";
+        }
+        if (tillHour > 12) {
+            tillHour -= 12;
+            tillSummary += String.valueOf(tillHour) + ":" + tillValues[1] + " PM";
+        } else {
+            tillSummary = times[0].substring(1) + " AM";
+        }
+        mSincePref.setSummary(sinceSummary);
+        mTillPref.setSummary(tillSummary);
+    }
+
+    private void updateTimeSetting(boolean since, int hour, int minute) {
+        String[] times = getCustomTimeSetting();
+        String nHour = "";
+        String nMinute = "";
+        if (hour < 10) nHour += "0";
+        if (minute < 10) nMinute += "0";
+        nHour += String.valueOf(hour);
+        nMinute += String.valueOf(minute);
+        times[since ? 0 : 1] = nHour + ":" + nMinute;
+        Settings.Secure.putStringForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_TIME,
+                times[0] + "," + times[1], UserHandle.USER_CURRENT);
+        updateTimeSummary(times, 2);
     }
 
     @Override
